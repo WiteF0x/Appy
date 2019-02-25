7b51bd215076c842539a4187f7b3d991abdf1b23
tommys <tommys@redwerk.com>
2019-02-25 12:24:40 +0300

    ---

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View, } from 'react-native';
+// import { NavigationActions } from 'react-navigation';
+// import Navigator from '../config/routes';
+
+import FetchLocation from './components/FetchLocation';
+
 
 export default class App extends React.Component {
+  getUserLocationHandler = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+      console.log(position)
+    }, err => console.log(err));
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Hello man!</Text>
+        <FetchLocation onGetLocation = {this.getUserLocationHandler} />
+        <View>
+          {ConsolePanel}
+        </View>
       </View>
     );
   }
